bc5962a925b823a14537020b584a263ad3f6e934
miststlkr <cs22raptors>
2018-08-17 22:41:55 -0700

    Added code for equipment in use; there is a bug with the speechText always responding to the previous utterance
    Utterance:  Ask VHS if they are open
    Response: Sorry, I didn't understand the command.
    U: Ask VHS if they are open
    R: Yes, we are open
    U: Ask VHS if the laser is free
    R: Yes we are open
    U: Ask VHS if they are open
    R: The laser is currently off
    
    I think the issue is a scope issue with the getWebRequest function returning speechText

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -3,24 +3,24 @@
 
 const Alexa = require('ask-sdk-core');
 const https = require('https');
- 
+
 const noList = ["Sorry", "Unfortunately", "Sadly", "Alas", "I\'m sad to say that", "Out of luck", "No", "Nope", "What a bummer", "My magic eight ball says"];
 const yesList = ["Yes", "Thankfully", "Indeed", "Yup", "I\'m happy to report that", "In fact", "Luckily", "What do you know", "Look at that", "Your stars have aligned", "Sure"];
 
 function getWebRequest(url,doWebRequestCallBack) {
     https.get(url, function (res) {
         var webResponseString = '';
- 
+
         if (res.statusCode != 200) {
             doWebRequestCallBack(new Error("Non 200 Response"));
         }
- 
+
         res.on('data', function (data) {
             webResponseString += data;
         });
- 
+
         res.on('end', function () {
-            var webResponseObject = JSON.parse(webResponseString);            
+            var webResponseObject = JSON.parse(webResponseString);
             if (webResponseObject.error) {
                 doWebRequestCallBack(new Error(webResponseObject.error.message));
             } else {
@@ -39,131 +39,158 @@ function randReply (options) {
 }
 
 const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Hack the planet!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hack the planet!';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('VHS', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
 };
 
 const IsVHSOpenIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'IsVHSOpenIntent';
-  },
-  handle(handlerInput) {
-    let url = "https://api.vanhack.ca/s/vhs/data/door.json";
-    
-    getWebRequest(url, function webResonseCallback(err, data) {
-        if (err) {
-            speechText = "Sorry I couldn't connect to the server: " + err;
-        } else {
-            const doorStatus = data.value;
-            if (doorStatus == 'open') {
-                speechText = randReply(yesList);
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'IsVHSOpenIntent';
+    },
+    handle(handlerInput) {
+        let url = "https://api.vanhack.ca/s/vhs/data/door.json";
+
+// HELP:  I think I have a scope issue with speechText here
+
+        getWebRequest(url, function webResonseCallback(err, data) {
+            if (err) {
+                speechText = "Sorry I couldn't connect to the server: " + err;
             } else {
-                speechText = randReply(noList);
+                const doorStatus = data.value;
+                if (doorStatus === 'open') {
+                    speechText = randReply(yesList);
+                } else {
+                    speechText = randReply(noList);
+                }
+                speechText += ', we are ' + doorStatus;
             }
-        speechText += ', we are ' + doorStatus;
-        }
-    });    
-    
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('VHS', speechText)
-      .getResponse();
-  },
+        });
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
+};
+
+const EquipmentUseIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'EquipmentUseIntent';
+    },
+    handle(handlerInput) {
+        let url = "https://api.vanhack.ca/s/vhs/data/laser.json";
+
+        getWebRequest(url, function webResonseCallback(err, data) {
+            if (err) {
+                const speechText = "Sorry I couldn't connect to the server: " + err;
+            } else {
+                const EquipmentStatus = data.value;
+                const speechText = 'Currently the laser is ' + EquipmentStatus;
+            }
+        });
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
 };
 
 const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello Hacker!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'Hello Hacker!';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('VHS', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
 };
 
 const HelpIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'You can say hello to me!  Of course if you want to put me to work, you can ask if VHS is open as well.';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me!  Of course if you want to put me to work, you can ask if VHS is open as well.';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('VHS', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
 };
 
 const CancelAndStopIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    const speechText = 'Happy hacking!';
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = 'Happy hacking!';
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('VHS', speechText)
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withSimpleCard('VHS', speechText)
+            .getResponse();
+    },
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  },
+        return handlerInput.responseBuilder.getResponse();
+    },
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, I didn\'t understand the command. Please try again.')
-      .reprompt('Sorry, I didn\'t understand the command. Please try again.')
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak('Sorry, I didn\'t understand the command. Please try again.')
+            .reprompt('Sorry, I didn\'t understand the command. Please try again.')
+            .getResponse();
+    },
 };
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    LaunchRequestHandler,
-    IsVHSOpenIntentHandler,
-    HelloWorldIntentHandler,
-    HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        IsVHSOpenIntentHandler,
+        EquipmentUseIntentHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
